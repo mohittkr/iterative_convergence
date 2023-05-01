8eefe323f723011ce1b2666b625694534c31e7f0
Mohit Tekriwal <tmohit@umich.edu>
2023-05-01 16:37:06 -0400

    added a theorem for limit of x

--- x_limit.v
+++ x_limit.v
@@ -27,6 +27,30 @@ Require Import complex_mat_vec_prop iter_necessity matrix_norm iter_convergence.
 Import ComplexField.
 
 
+(**
+Theorem iter_convergence: 
+  forall (n:nat) (A: 'M[R]_n.+1) (b: 'cV[R]_n.+1)
+  (A1 A2 : 'M[R]_n.+1), 
+  A \in unitmx ->
+   A1 \in unitmx ->
+   A = A1 + A2 ->
+   let x := (invmx A) *m b in
+   (let S_mat:= RtoC_mat (- ( A1^-1 *m A2)) in 
+     (forall (i: 'I_n.+1), (C_mod (lambda S_mat i) < 1)%Re)) <->
+    (forall x0: 'cV[R]_n.+1,
+        is_lim_seq (fun m:nat => vec_norm ((X_m m.+1 x0 b A1 A2) - x)) 0%Re).
 
+**)
 
-
+Theorem x_limit_eq: 
+  forall (n:nat) (A: 'M[R]_n.+1) (b: 'cV[R]_n.+1)
+  (A1 A2 : 'M[R]_n.+1), 
+   A \in unitmx ->
+   A1 \in unitmx ->
+   A = A1 + A2 ->
+   let x := (invmx A) *m b in
+   (let S_mat:= RtoC_mat (- ( A1^-1 *m A2)) in 
+     (forall (i: 'I_n.+1), (C_mod (lambda S_mat i) < 1)%Re)) ->
+   (forall x0: 'cV[R]_n.+1,
+      vec_norm x = Lim_seq (fun m:nat => vec_norm (X_m m.+1 x0 b A1 A2))).
+Admitted.
